733e70e6fe5fef243ba4b6a8a9e3627f3b3b4f04
Si Hyeong Lee <super2451894@gmail.com>
2022-09-16 17:01:46 +0900

    Update test.js

--- tools/test.js
+++ tools/test.js
@@ -1,12 +1,128 @@
+const glob = require("glob");
 const fs = require("fs");
 
-const dir =
-  "G:\\공유 드라이브\\users.parks98.07.NETPLEX\\3.요청 자료\\예능(이시형님)"; // 지정 경로
+const rootFolder = "/mnt/c/Users/super/Documents/GitHub/netplex/dir"; // 최상위 경로
+// const trashFolder = "/mnt/c/Users/super/Documents/GitHub/netplex/trash"; // 휴지통 경로
+const destinationFolder =
+  "/mnt/c/Users/super/Documents/GitHub/netplex/destination"; // 이동할 경로
 
-fs.readdir(dir, async function (error, fileList) {
-  if (error) {
-    console.log(error); // 오류 발생 시 에러 출력
+const setSeries = [
+  {
+    seriseName: "1박 2일",
+    additionalName: ["1박 2일", "1박2일", "1박-2일"],
+  },
+  {
+    seriseName: "여름특집 놀면 뭐하니",
+    additionalName: ["여름특집 놀면 뭐하니", "여름특집놀면뭐하니"],
+  },
+  {
+    seriseName: "나 혼자 산다",
+    additionalName: ["나 혼자 산다", "나혼자산다"],
+  },
+  {
+    seriseName: "런닝맨",
+    additionalName: ["런닝맨"],
+  },
+  {
+    seriseName: "아는 형님",
+    additionalName: ["아는 형님", "아는형님"],
+  },
+  {
+    seriseName: "전지적 참견 시점",
+    additionalName: ["전지적 참견 시점", "전지적참견시점"],
+  },
+];
+
+// const log = rootFolder + "/result.txt"; // 로그파일 설정
+// fs.open(log, "w", (err, fd) => {
+//   if (err) throw err;
+//   console.log("result.txt 파일 생성 완료");
+// }); // 로그파일 생성
+
+/* setFolder 에 설정된 폴더 생성*/
+setSeries.forEach((folder) => {
+  if (!fs.existsSync(destinationFolder + "/" + folder.seriseName)) {
+    fs.mkdirSync(destinationFolder + "/" + folder.seriseName);
   }
-  console.log(fileList); // dir 경로에 있는 파일 및 폴더명 출력)
 });
-//   fileList = fileList.filter((except) => !exceptFileAndFolder.includes(except)); // 정리완료 폴더와 result.log 파일 제외
+
+let selectExtention = ["mp4", "mkv", "avi"]; // 확장자 설정
+let selectAllFiles = `${rootFolder}/**/*`;
+// 경로를 `/mnt/c/Users/super/Documents/GitHub/netplex/dir/**/*(*.mp4|*.mkv)` 와 같은 형식으로 설정 완료
+selectExtention = selectExtention.map((ext) => `*.${ext}`).join("|");
+
+const searchDir = `${rootFolder}/**/*(${selectExtention})`;
+
+async function main() {
+  await glob(searchDir, (err, files) => {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log(files);
+      for (let i = 0; i < files.length; i++) {
+        for (let j = 0; j < setSeries.length; j++) {
+          if (files[i].includes(setSeries[j].seriseName)) {
+            fs.rename(
+              files[i],
+              destinationFolder +
+                "/" +
+                setSeries[j].seriseName +
+                "/" +
+                files[i].split("/").pop(),
+              (err) => {
+                if (err) throw err;
+                console.log(`일치 파일 처리 완료 : ${files[i]}`);
+              }
+            );
+            break;
+          } else if (
+            setSeries[j].additionalName.includes(
+              files[i].split("/").pop().split(".")[0]
+            )
+          ) {
+            fs.rename(
+              files[i],
+              destinationFolder +
+                "/" +
+                setSeries[j].seriseName +
+                "/" +
+                files[i].split("/").pop(),
+              (err) => {
+                if (err) throw err;
+                console.log(`예외 파일 처리 완료 : ${files[i]}`);
+              }
+            );
+            break;
+          }
+        }
+      }
+    }
+  });
+
+  //   /* 남은 파일 및 폴더 정리 */
+  //   await glob(selectAllFiles, (err, files) => {
+  //     if (err) {
+  //       console.log("Error", err);
+  //     } else {
+  //       for (let i = 0; i < files.length; i++) {
+  //         fs.rename(
+  //           files[i],
+  //           trashFolder + "/" + files[i].split("/").pop(),
+  //           (err) => {
+  //             console.log(`남은 파일 처리 완료 : ${files[i]}`);
+  //           }
+  //         );
+  //       }
+  //     }
+  //   });
+
+  //   await glob("", { matchBase: true }, (err, files) => {
+  //     if (err) {
+  //       console.log("Error", err);
+  //     } else {
+  //       console.log(files);
+  //     }
+  //   });
+}
+
+main();
