b5a1e021590f1c73a6bcdc8516cf83a5ff71646d
Si Hyeong Lee <super2451894@gmail.com>
2022-09-17 12:55:06 +0900

    원본 폴더 다중 선택 가능하도록 변경

--- tools/test.js
+++ tools/test.js
@@ -1,7 +1,7 @@
 const glob = require("glob");
 const fs = require("fs");
 
-const rootFolder = "/mnt/c/Users/super/Documents/GitHub/netplex/dir"; // 최상위 경로
+const rootFolder = ["/mnt/c/Users/super/Documents/GitHub/netplex/dir", ""]; // 최상위 경로
 // const trashFolder = "/mnt/c/Users/super/Documents/GitHub/netplex/trash"; // 휴지통 경로
 const destinationFolder =
   "/mnt/c/Users/super/Documents/GitHub/netplex/destination"; // 이동할 경로
@@ -152,11 +152,6 @@ const setSeries = [
     additionalName: ["집사부일체"],
   },
 ];
-// const log = rootFolder + "/result.txt"; // 로그파일 설정
-// fs.open(log, "w", (err, fd) => {
-//   if (err) throw err;
-//   console.log("result.txt 파일 생성 완료");
-// }); // 로그파일 생성
 
 /* setFolder 에 설정된 폴더 생성*/
 setSeries.forEach((folder) => {
@@ -166,82 +161,57 @@ setSeries.forEach((folder) => {
 });
 
 let selectExtention = ["mp4", "mkv", "avi"]; // 확장자 설정
-let selectAllFiles = `${rootFolder}/**/*`;
 // 경로를 `/mnt/c/Users/super/Documents/GitHub/netplex/dir/**/*(*.mp4|*.mkv)` 와 같은 형식으로 설정 완료
 selectExtention = selectExtention.map((ext) => `*.${ext}`).join("|");
 
-const searchDir = `${rootFolder}/**/*(${selectExtention})`;
-
 async function main() {
-  await glob(searchDir, (err, files) => {
-    if (err) {
-      console.log("Error", err);
-    } else {
-      console.log(files);
-      for (let i = 0; i < files.length; i++) {
-        for (let j = 0; j < setSeries.length; j++) {
-          if (files[i].startsWith(setSeries[j].seriseName)) {
-            fs.rename(
-              files[i],
-              destinationFolder +
-                "/" +
-                setSeries[j].seriseName +
-                "/" +
-                files[i].split("/").pop(),
-              (err) => {
-                if (err) throw err;
-                console.log(`일치 파일 처리 완료 : ${files[i]}`);
-              }
-            );
-            break;
-          } else if (
-            setSeries[j].additionalName.startsWith(
-              files[i].split("/").pop().split(".")[0]
-            )
-          ) {
-            fs.rename(
-              files[i],
-              destinationFolder +
-                "/" +
-                setSeries[j].seriseName +
-                "/" +
-                files[i].split("/").pop(),
-              (err) => {
-                if (err) throw err;
-                console.log(`예외 파일 처리 완료 : ${files[i]}`);
-              }
-            );
-            break;
+  for (let k = 0; k < rootFolder.length; k++) {
+    const searchDir = `${rootFolder[k]}/**/*(${selectExtention})`;
+    await glob(searchDir, (err, files) => {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log(files);
+        for (let i = 0; i < files.length; i++) {
+          for (let j = 0; j < setSeries.length; j++) {
+            if (files[i].startsWith(setSeries[j].seriseName)) {
+              fs.rename(
+                files[i],
+                destinationFolder +
+                  "/" +
+                  setSeries[j].seriseName +
+                  "/" +
+                  files[i].split("/").pop(),
+                (err) => {
+                  if (err) throw err;
+                  console.log(`일치 파일 처리 완료 : ${files[i]}`);
+                }
+              );
+              break;
+            } else if (
+              setSeries[j].additionalName.startsWith(
+                files[i].split("/").pop().split(".")[0]
+              )
+            ) {
+              fs.rename(
+                files[i],
+                destinationFolder +
+                  "/" +
+                  setSeries[j].seriseName +
+                  "/" +
+                  files[i].split("/").pop(),
+                (err) => {
+                  if (err) throw err;
+                  console.log(`예외 파일 처리 완료 : ${files[i]}`);
+                }
+              );
+              break;
+            }
           }
         }
       }
-    }
-  });
-
-  //   /* 남은 파일 및 폴더 정리 */
-  //   await glob(selectAllFiles, (err, files) => {
-  //     if (err) {
-  //       console.log("Error", err);
-  //     } else {
-  //       for (let i = 0; i < files.length; i++) {
-  //         fs.rename(
-  //           files[i],
-  //           trashFolder + "/" + files[i].split("/").pop(),
-  //           (err) => {
-  //             console.log(`남은 파일 처리 완료 : ${files[i]}`);
-  //           }
-  //         );
-  //       }
-  //     }
-  //   });
-
-  //   await glob("", { matchBase: true }, (err, files) => {
-  //     if (err) {
-  //       console.log("Error", err);
-  //     } else {
-  //       console.log(files);
-  //     }
-  //   });
+    });
+  }
 }
 
 main();
